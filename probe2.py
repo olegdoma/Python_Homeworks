640b05dc56f13871e3027ed375ccc886cf30cd3f
olegdoma <olegdoma@yandex.ru>
2020-07-29 17:26:26 +0300

    HW
    Задание 12

--- probe2.py
+++ probe2.py
@@ -1,14 +1,8 @@
-num = int(input())
-for i in range(2, num):
-    list_num = list[num % i]
-    for j in list_num:
-        if list_num[j] == 0:
-            print('Число составное')
-        else:
-            print('Число простое')
-    print(list_num)
-
-#if a == 1:
-#    print('Число простое')
-#else:
-#    print('Число составное')
+n = 5
+list_n = []
+i = 0
+for i in range(n-1):
+    list_n.append(' ')
+    i += 1
+list_n.insert(2, '*')
+print(list_n)
